1b5cb9048b6b316bafcb2d49ed314b09ecad600e
Nestor Fernandez <extraertond@gmail.com>
2022-12-19 02:44:02 +0100

    Initial configuration and all necesaries dependencies have been installed

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./index.html",
   filename: "./index.html",
+  favicon: "./favicon.ico",
 });
 const Dotenv = require("dotenv-webpack");
 
@@ -10,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|ts|tsx)/,
+        test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -19,6 +20,14 @@ module.exports = {
       {
         test: /\.(c|sc|sa)ss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
       },
     ],
   },
