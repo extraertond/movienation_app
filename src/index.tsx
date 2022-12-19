0de138ec41434e60850d80446e01b07b28c56de1
Nestor Fernandez <extraertond@gmail.com>
2022-12-19 02:48:37 +0100

    Configured generic application styles and override MUI styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "./services/i18n";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<div>Movienation App</div>);
+root.render(<App />);
